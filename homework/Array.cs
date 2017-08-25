992c5c13d01176eb9087708d88a09ab14f17c717
zarin-raim <zarinar1105@gmail.com>
2017-08-26 00:44:23 +0600

    Change readme, hw

---
+++ homework/Array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace homework
+{
+    public class Array 
+    {
+        private int[] _array;
+      
+        public Array()
+        {
+            Size = 1;
+            _array = new int[Size];
+        }
+
+        public Array(int size)
+        {
+            Size = size;
+            _array = new int[Size];
+        }
+
+        public int Size { get; set; }
+
+        public int this[int i]
+        {
+            get
+            {
+                return _array[i];
+            }
+            set
+            {
+                _array[i] = (int)Math.Pow((double)value, 2);
+            }
+        }
+
+    }
+}
---
+++ homework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace homework
+{
+    class Program
+    {
+        /*
+         * Создать индексатор для одномерного массива который при установке значения будет 
+         * возводить в квадрат передаваемое значение переменной и устанавливать его для указанного индекса. э
+         * При получении элемента массива по индексу будет возвращаться его текущее значение.
+         */
+
+        static void Main(string[] args)
+        {
+            Array array = new Array(3);
+            array[0] = 4;
+            array[1] = 134;
+            array[2] = 3456;
+
+            for(int i = 0; i < array.Size; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
